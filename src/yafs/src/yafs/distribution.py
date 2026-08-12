fde87227bf6488cb7fdd86877a09d094f92bb548
Ildefons Magrans de Abril <ildefons.magrans@gmail.com>
2026-08-12 16:10:26 +0200

    Add Gamma instruction demand distribution

--- src/yafs/src/yafs/distribution.py
+++ src/yafs/src/yafs/distribution.py
@@ -91,3 +91,35 @@ class uniformDistribution(Distribution):
         super(uniformDistribution, self).__init__(**kwargs)
     def next(self):
         return random.randint(self.min, self.max)
+
+# ILDEBEGINE PRAISE
+class gamma_distribution(Distribution):
+    """
+    Gamma-distributed positive values parameterized by mean
+    and coefficient of variation (CV).
+    """
+
+    def __init__(self, mean, cv, seed=1, **kwargs):
+        if mean <= 0:
+            raise ValueError("mean must be positive")
+        if cv <= 0:
+            raise ValueError("cv must be positive")
+
+        super(gamma_distribution, self).__init__(**kwargs)
+
+        self.mean = mean
+        self.cv = cv
+
+        # Gamma parameters
+        self.shape = 1.0 / (cv ** 2)
+        self.scale = mean * (cv ** 2)
+
+        self.rnd = np.random.RandomState(seed)
+
+    def next(self):
+        value = self.rnd.gamma(
+            shape=self.shape,
+            scale=self.scale
+        )
+        return max(1, int(value))
+#ILDEEND
